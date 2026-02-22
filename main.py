5d85195102c04370d2de0c086ba384ed956890c0
RedKrane86 <redkrane86@gmail.com>
2026-02-22 19:51:14 +0500

    Big reformat

--- main.py
+++ main.py
@@ -1,64 +1,35 @@
 from utils import book_list_view, add_book, remove_book, issue_book, return_book, find_book
 
 
-def main(library):
-
-    run_program = True
+def main(library, main_menu):
     print('Добро пожаловать в нашу библиотеку!')
 
-    while run_program:
-        print('\n1. Просмотр всех книг\n'
-              '2. Добавить книгу в библиотеку\n'
-              '3. Удалить книгу из библиотеки\n'
-              '4. Выдать книгу читателю\n'
-              '5. Вернуть книгу от читателя\n'
-              '6. Найти книгу по названию\n'
-              '7. Выход. Остановить программу\n'
-              )
+    while True:
         try:
-            user_input = int(input('Выберете интересующий пункт меню (1 - 7):\n'))
+            for operation in main_menu:
+                print(*operation)
 
-            if user_input == 1:
-                book_list_view(library)
+            user_input = int(input('Выберете интересующий пункт меню (1 - 0):\n'))
 
-            elif user_input == 2:
-                new_book_title = input('Введите название новой книги:\n')
-                new_book_author = input('Введите автора новой книги:\n')
-                new__book_year = input('Введите год публикации новой книги:\n')
-
-                add_book(new_book_title, new_book_author, new__book_year, library)
-
-            elif user_input == 3:
-                to_remove_book_title = input('Введите название книги на удаление:\n')
-                remove_book(to_remove_book_title, library)
-
-            elif user_input == 4:
-                issue_book_title = input('Введите название книги на выдачу:\n')
-                issue_book(issue_book_title, library)
-
-            elif user_input == 5:
-                return_book_title = input('Введите название книги на возврат:\n')
-                return_book(return_book_title, library)
-
-            elif user_input == 6:
-                to_find_book_title = input('Введите название книги для поиска:\n')
-                find_book(to_find_book_title, library)
-
-            elif user_input == 7:
+            if user_input == 0:
                 print('Выход из программы... Не отключайте компьютер от сети...')
-                run_program = False
+                break
+
+            for line in main_menu.keys():
+                if user_input in line:
+                    main_menu[line](library)
+                    break
 
             else:
                 print('\nВведена не существующая команда\n'
-                      'Воспользуйтесь цифрами от 1 до 7\n'
-                      'или обратитесь к системному администратору, если проблема сохранилась')
+                      'Воспользуйтесь цифрами от 1 до 0\n'
+                      'или обратитесь к системному администратору, если проблема сохранилась\n')
 
         except ValueError:
-            print('\nВведена не существующая команда\n'
-                'Воспользуйтесь цифровой клавиатурой от 1 до 7\n'
-                'или обратитесь к системному администратору, если проблема сохранилась'
+            print('\nОшибка\n'
+                'Воспользуйтесь цифровой клавиатурой от 1 до 0\n'
+                'или обратитесь к системному администратору, если проблема сохранилась\n'
             )
-
 
 library_dict = {
     'Гарри Поттер и философский камень': {
@@ -73,6 +44,16 @@ library_dict = {
     }
 }
 
+main_menu_dict = {
+    (1, '- Просмотр всех книг'): book_list_view,
+    (2, '- Добавить книгу в библиотеку'): add_book,
+    (3, '- Удалить книгу из библиотеки'): remove_book,
+    (4, '- Выдать книгу читателю'): issue_book,
+    (5, '- Вернуть книгу от читателя'): return_book,
+    (6, '- Найти книгу по названию'): find_book,
+    (0, '- Выход из программы'): None
+}
+
 
 if __name__ == '__main__':
-    main(library_dict)
+    main(library_dict, main_menu_dict)
--- utils.py
+++ utils.py
@@ -18,106 +18,118 @@ def book_list_view(library: dict):
         print('В данный момент в библиотеке нет книг')
 
 
-def add_book(title: str, author: str, year: str, library: dict) -> dict:
+def add_book(library: dict) -> dict:
     """
     - Добавляет книгу в словарь library_dict
     - Если книга с таким названием уже существует, обновляет информацию о ней
     - Возвращает словарь library_dict
     """
+    new_book_title = input('Введите название новой книги:\n')
+    new_book_author = input('Введите автора новой книги:\n')
+    new__book_year = input('Введите год публикации новой книги:\n')
+
     try:
         new_book_dict = {
-            title: {
-                'author': author.title(),
-                'publication_year': int(year),
+            new_book_title: {
+                'author': new_book_author.title(),
+                'publication_year': int(new__book_year),
                 'is_available': None
             }
         }
 
-        if title in library:
-            update_choice = input(f'\nКнига "{title}" уже есть в библиотеке.\n'
+        if new_book_title in library:
+            update_choice = input(f'\nКнига "{new_book_title}" уже есть в библиотеке.\n'
                                   'Обновить информацию о ней? (да/нет):\n')
 
             if update_choice.lower() == 'да':
-                library[title] = new_book_dict
+                library[new_book_title] = new_book_dict
                 print('Информация о книге обновлена\n')
             else:
                 print('Книга в библиотеке осталась в первозданном виде. Новая книга удалена\n')
             return library
 
         library.update(new_book_dict)
-        print(f'Книга "{title}" успешно добавлена в библиотеку\n')
+        print(f'Книга "{new_book_title}" успешно добавлена в библиотеку\n')
         return library
 
     except ValueError:
         print('Ошибка. Передан неподходящий тип данных\n')
 
 
-def remove_book(title: str, library: dict) -> dict:
+def remove_book(library: dict) -> dict:
     """
     - Удаляет книгу из словаря library_dict
     - Если книга не найдена, выводит сообщение об этом
     - Возвращает словарь library_dict
     """
-    if title in library:
-        del library[title]
-        print(f'Книга "{title}" успешно удалена из библиотеки\n')
+    to_remove_book_title = input('Введите название книги на удаление:\n')
+
+    if to_remove_book_title in library:
+        del library[to_remove_book_title]
+        print(f'Книга "{to_remove_book_title}" успешно удалена из библиотеки\n')
     else:
-        print(f'Кажется, книги "{title}" нет в нашей библиотеке. Операция отменена.\n')
+        print(f'Кажется, книги "{to_remove_book_title}" нет в нашей библиотеке. Операция отменена.\n')
     return library
 
 
-def issue_book(title: str, library: dict) -> dict:
+def issue_book(library: dict) -> dict:
     """
     - Отмечает книгу как выданную, 'is_available' становится 'False'
     - Возвращает словарь library_dict
     """
-    if title in library:
+    issue_book_title = input('Введите название книги на выдачу:\n')
 
-        if library[title]['is_available']:
-            library[title]['is_available'] = False
-            print(f'Книга "{title}" успешно выдана\n')
+    if issue_book_title in library:
+
+        if library[issue_book_title]['is_available']:
+            library[issue_book_title]['is_available'] = False
+            print(f'Книга "{issue_book_title}" успешно выдана\n')
         else:
-            print(f'Книга {title} уже выдана. Попробуйте в другой день\n')
+            print(f'Книга {issue_book_title} уже выдана. Попробуйте в другой день\n')
 
     else:
-        print(f'Кажется, книги "{title}" нет в нашей библиотеке. Операция отменена.\n')
+        print(f'Кажется, книги "{issue_book_title}" нет в нашей библиотеке. Операция отменена.\n')
     return library
 
 
-def return_book(title: str, library: dict) -> dict:
+def return_book(library: dict) -> dict:
     """
     - Отмечает книгу как вернувшуюся в библиотеку, 'is_available' становится 'True'
     - Возвращает словарь library_dict
     """
-    if title in library:
+    return_book_title = input('Введите название книги на возврат:\n')
 
-        if not library[title]['is_available']:
-            library[title]['is_available'] = True
-            print(f'Книга "{title}" успешно возвращена\n')
+    if return_book_title in library:
+
+        if not library[return_book_title]['is_available']:
+            library[return_book_title]['is_available'] = True
+            print(f'Книга "{return_book_title}" успешно возвращена\n')
         else:
-            print(f'Книгу {title} уже вернули. Кажется, вы ошиблись библиотекой\n')
+            print(f'Книгу {return_book_title} уже вернули. Кажется, вы ошиблись библиотекой\n')
 
     else:
-        print(f'Кажется, книги "{title}" нет в нашей библиотеке. Операция отменена.\n')
+        print(f'Кажется, книги "{return_book_title}" нет в нашей библиотеке. Операция отменена.\n')
     return library
 
 
-def find_book(title: str, library: dict):
+def find_book(library: dict):
     """
     Процедура
     - Выводит информацию о книге по ее названию
     - Если книга не найдена, выводит соответствующее сообщение
     """
-    if title in library:
-        print(f'Название: {title}\n'
-              f'Автор: {library[title]["author"]}\n'
-              f'Дата публикации: {library[title]["publication_year"]}')
+    to_find_book_title = input('Введите название книги для поиска:\n')
 
-        if library[title]["is_available"] is None:
-            print('Книга в библиотеке, но ее статус не определен')
-        elif library[title]["is_available"]:
-            print('Книга доступна')
-        elif not library[title]["is_available"]:
-            print('Книга выдана')
+    if to_find_book_title in library:
+        print(f'\nНазвание: {to_find_book_title}\n'
+              f'Автор: {library[to_find_book_title]["author"]}\n'
+              f'Дата публикации: {library[to_find_book_title]["publication_year"]}')
+
+        if library[to_find_book_title]["is_available"] is None:
+            print('Книга в библиотеке, но ее статус не определен\n')
+        elif library[to_find_book_title]["is_available"]:
+            print('Книга доступна\n')
+        elif not library[to_find_book_title]["is_available"]:
+            print('Книга выдана\n')
     else:
-        print(f'Кажется, книги "{title}" нет в нашей библиотеке. Операция отменена.\n')
+        print(f'Кажется, книги "{to_find_book_title}" нет в нашей библиотеке. Операция отменена.\n')
